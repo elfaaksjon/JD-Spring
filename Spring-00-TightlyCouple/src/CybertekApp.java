658b99c69ae2e6ef7f9378558913e923adeab9b7
Adel <adel@gmail.com>
2020-10-19 00:03:11 +0100

    Loosely Couple

--- Spring-00-TightlyCouple/src/CybertekApp.java
+++ Spring-00-TightlyCouple/src/CybertekApp.java
@@ -7,6 +7,20 @@ public class CybertekApp {
 
         Mentor mentor = new Mentor(fullTime,partTime);
         mentor.manageAccount();
+        /* what is Coupling?
+        Usage of an object by another object.
+        it is simple mmeans that one object requires another object to complete its assigned task.
+        There are two ypes of coupling :
+            1. Tight coupling
+                A group of classes are highly dependent on one another
+                A class has a direct reference to a concrete class.
+            2Loose Cpupling
+                An object getes the object to be used from external sources
+                Reduces the dependencies of a class that uses the diffenet classes directly
+
+
+            */
+
 
     }
 }
