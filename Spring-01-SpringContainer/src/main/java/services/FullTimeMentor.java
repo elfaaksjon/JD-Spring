1c4bf4ce9de966f81e967884a83f4b357b2cce1d
Adel <adel@gmail.com>
2020-10-19 01:44:45 +0100

    Spring Container

---
+++ Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
@@ -0,0 +1,13 @@
+package services;
+
+import interfaces.Mentor;
+
+public class FullTimeMentor implements Mentor {
+
+    @Override
+    public void createAccount(){
+        System.out.println("Full Time Mentor Account is created");
+
+    }
+
+}
