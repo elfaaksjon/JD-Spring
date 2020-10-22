e7a2bce0637037d052fdb9176954e89761553c97
Adel <adel@gmail.com>
2020-10-22 11:34:28 +0100

    DI-Setter Injection

--- Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/OfficeHourse.java
+++ Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/OfficeHourse.java
@@ -1,6 +1,7 @@
 package com.cybertek.services;
 
-import com.cybertek.Interfaces.ExtraSessions;
+
+import com.cybertek.interfaces.ExtraSessions;
 
 public class OfficeHourse  implements ExtraSessions {
     @Override
