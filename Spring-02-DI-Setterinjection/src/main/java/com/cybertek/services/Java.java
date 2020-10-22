cc448ce3ebeb9ae8641dcc139b141df45114300a
Adel <adel@gmail.com>
2020-10-22 11:33:29 +0100

    DI-Setter Injection

---
+++ Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,21 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java  implements Course {
+
+    //OfficeHourse officeHourse;
+
+    ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    public void getTeachingHours() {
+
+        System.out.println("Weekly Teaching Hours : " + (20+extraSessions.getHours())  );
+
+    }
+}
