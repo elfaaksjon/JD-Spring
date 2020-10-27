3c609651640dd30997ffdf26ec10d1a01342c921
Adel <adel@gmail.com>
2020-10-27 12:55:14 +0000

    Java Annotations-Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,37 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.util.Arrays;
+
+
 public class Java implements Course {
 
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours 30");
+        System.out.println("Weekly teaching hours :" + extraSessions.getHours());
+    }
+    @Value("JDI")
+    private  String batch;
+    @Value("${instructor}")
+    private String instructor;
+    @Value("${days")
+    private String [] days;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
     }
 }
